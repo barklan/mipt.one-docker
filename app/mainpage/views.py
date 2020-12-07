715a1d6a21209a2341621cbe221e4373f1954f5e
docker-srv <buzin.gb@phystech.edu>
2020-12-07 11:50:32 +0000

    working with kor GET method

--- app/mainpage/views.py
+++ app/mainpage/views.py
@@ -1,10 +1,44 @@
 from django.shortcuts import render
 from django.core.files.storage import FileSystemStorage
 from django.shortcuts import redirect
+from aiohttp import ClientSession
+from bs4 import BeautifulSoup
+import asyncio
+
 
 
 def mainpage(request):
-    return render(request, 'mainpage/index.html')
+    try:
+        sem = request.GET['sem']
+        zad = request.GET['zad']
+        url = f'https://mipt1.ru/1_2_3_4_5_kor.php?sem={sem}&zad={zad}'
+
+
+        async def fetch_links_and_pass(url: str, session: ClientSession):
+            async with session.get(url) as response:
+                html = await response.text()
+            soup = BeautifulSoup(html, 'html.parser')
+            div = soup.find_all("div", class_="short_content")[0]
+            output = div.b.get_text()
+            return output
+
+
+        async def fetch(url):
+            async with ClientSession() as session:
+                tasks = [asyncio.create_task(fetch_links_and_pass(url, session))]
+                output = await asyncio.gather(*tasks)
+                return output
+
+
+        output_list = asyncio.run(fetch(url))
+        output = output_list[0]
+
+        context = {
+            'search_output': output,
+        }
+        return render(request, 'mainpage/index.html', context)
+    except:
+        return render(request, 'mainpage/index.html')
 
 
 # def image_upload(request):
