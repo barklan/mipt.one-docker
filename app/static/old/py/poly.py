124fdf95429348c0245c691538170caa91b4ac2f
docker-srv <buzin.gb@phystech.edu>
2020-12-22 15:51:55 +0000

    delete depricated static folder

--- app/static/old/py/poly.py
+++
@@ -1,8 +0,0 @@
-def polyhash(s, p, m):
-    lenm = len(s)
-    h = pow(p,lenm-1)%m
-    ans = 0
-    result = []
-    for i in range(lenm): # preprocessing
-        ans = (p*ans+ord(s[i]))%m
-    return ans
