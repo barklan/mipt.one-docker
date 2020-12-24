457e37b3949d6f03618ba121094a5c527e4ccbb2
docker-srv <buzin.gb@phystech.edu>
2020-12-24 04:08:15 +0000

    working version

--- aiogram/miptone_bot.py
+++ aiogram/miptone_bot.py
@@ -1,11 +1,7 @@
-# this is for local development
-# from tgbottoken import TGBOTTOKEN as API_TOKEN
-
-# this is for production
 import os
 API_TOKEN = os.environ.get('TGBOTTOKEN', 'none')
+import quote
 
-import logging
 
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.types.message import ContentType
@@ -16,35 +12,31 @@ import ssl
 ssl._create_default_https_context = ssl._create_unverified_context
 
 
+import logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
+
 # Initialize bot and dispatcher
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
-
 sem_dict = {}
+
+
+def change_sem_keyboard():
+    keyboard_markup = types.InlineKeyboardMarkup(row_width=3)
+    text_and_data = ((str(i), str(i)) for i in range(1, 6))
+    row_btns = (types.InlineKeyboardButton(text, callback_data=data) for text, data in text_and_data)
+    keyboard_markup.row(*row_btns)
+    return keyboard_markup
+
 
 @dp.message_handler(commands='start')
 async def start_cmd_handler(message: types.Message):
-
-    keyboard_markup = types.InlineKeyboardMarkup(row_width=3)
-    text_and_data = (
-        ('1', '1'),
-        ('2', '2'),
-        ('3', '3'),
-        ('4', '4'),
-        ('5', '5'),
-    )
-    row_btns = (types.InlineKeyboardButton(text, callback_data=data) for text, data in text_and_data)
-    keyboard_markup.row(*row_btns)
-
-    await message.reply("Привет!\nУ меня есть много задач по физике!\nПросто выбери семестр и набери номер задачи.",
-                        reply_markup=keyboard_markup)
+    await message.reply("Привет!\nУ меня есть много задач по физике!\nПросто выбери семестр и набери номер задачи.\nТакже вы всегда можете выбрать семестр набрав 'семестр'", reply_markup=change_sem_keyboard())
 
 
-# Use multiple registrators. Handler will execute when one of the filters is OK
 @dp.callback_query_handler(text='1')
 @dp.callback_query_handler(text='2')
 @dp.callback_query_handler(text='3')
@@ -52,17 +44,13 @@ async def start_cmd_handler(message: types.Message):
 @dp.callback_query_handler(text='5')
 async def inline_kb_answer_callback_handler(query: types.CallbackQuery):
     answer_data = query.data
-    # always answer callback queries, even if you have nothing to say
     sem_dict[query.from_user.id] = answer_data
     await query.answer(f'Выбранный семестр: {answer_data!r}')
-
-
-    await bot.send_message(query.from_user.id, "Номер?")
+    await bot.send_message(query.from_user.id, "Номер задачи?")
 
 
 @dp.message_handler(regexp=r'^\d{1,2}\.\d{1,3}$')
 async def all_msg_handler(message: types.Message):
-    
     if message.from_user.id in sem_dict:
         sem = sem_dict[message.from_user.id]
         zad = message.text
@@ -70,8 +58,8 @@ async def all_msg_handler(message: types.Message):
         req = urllib.request.Request(url)
         response = urllib.request.urlopen(req)
         result = json.loads(response.read().decode())
-
         output = result['search_output']
+
         await message.answer(output)
 
         if result['image_found'] == True:
@@ -86,20 +74,19 @@ async def all_msg_handler(message: types.Message):
                 'Вы можете отправить своё решение',
             )
     else:
-        keyboard_markup = types.InlineKeyboardMarkup(row_width=3)
-        text_and_data = (
-            ('1', '1'),
-            ('2', '2'),
-            ('3', '3'),
-            ('4', '4'),
-            ('5', '5'),
-        )
-        row_btns = (types.InlineKeyboardButton(text, callback_data=data) for text, data in text_and_data)
-        keyboard_markup.row(*row_btns)
         await bot.send_message(
             message.from_user.id, 
             'Пожалуйста выберете семестр.\nВыбранный семестр мог сброситься при обновленни бота.',
-            reply_markup=keyboard_markup
+            reply_markup=change_sem_keyboard()
+        )
+
+
+@dp.message_handler(regexp=r'^семестр$')
+async def change_sem(message: types.Message):
+    await bot.send_message(
+            message.from_user.id, 
+            'Хотите сменить семестр?',
+            reply_markup=change_sem_keyboard()
         )
 
 
@@ -135,9 +122,14 @@ async def photo(message: types.Message):
 async def echo(message: types.Message):
     # old style:
     # await bot.send_message(message.chat.id, message.text)
-
-    await message.answer("Я не понимаю этого запроса.")
+    try:
+        answer = quote.get_quote(model)
+    except:
+        answer = 'Я не понимаю этот запрос :('
+    await message.answer(answer)
 
 
 if __name__ == '__main__':
+    global model
+    model = quote.fit_model()
     executor.start_polling(dp, skip_updates=True)
